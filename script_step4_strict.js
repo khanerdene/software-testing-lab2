bbb809c3adb396977b23be06eeb10d9afa07874b
khanerdene <khanerdene.email@gmail.com>
2026-09-15 21:39:33 +0800

    reran tests

---
+++ script_step4_strict.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+    vus: 30, duration: "1m",
+
+    thresholds: {
+        http_req_duration: ['p(95)<100'],
+        http_req_failed:   ['rate<0.01'],
+    }
+};
+
+export default function () {
+  const res = http.get('https://test.k6.io'); // -> https://quickpizza.grafana.com/
+  check(res, { '200 OK': (r) => r.status === 200 });
+  sleep(1);
+}
